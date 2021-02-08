a590b921156ad43639ff95d7e8d3d18454eb45c6
Astrid <40673001+Astrid777@users.noreply.github.com>
2021-02-08 20:55:19 +0500

    Add files via upload

---
+++ WpfApp20/Page1.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp20.Models;
+
+namespace WpfApp20
+{
+
+
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+
+        forecastContext db;
+        public event CancelEventHandler Closing;
+
+        private List<forecast> forecastsList;
+
+        public Page1()
+        {
+            InitializeComponent();
+
+            db = new forecastContext();
+            db.forecasts.Load(); // загружаем данные
+
+            forecastsList = db.forecasts.ToList();
+
+            forecastGrid.ItemsSource = db.forecasts.Local.ToBindingList(); // устанавливаем привязку к кэшу
+            Closing += MainWindow_Closing;
+
+            //выбрать список всех точек
+            comboBox1.ItemsSource = selectInventId();
+
+            //посчитать выборку
+            getCalculations(getIndex());
+
+        }
+
+        private List<string> selectInventId()
+        {
+            List<string> lstResult = forecastsList.Select(p => p.InventLocationId).Distinct().ToList();
+            lstResult.Add("Все");
+
+            return lstResult;
+        }
+
+
+        private List<forecast> getIndex()
+        {
+            List<forecast> forecasts = new List<forecast>();
+
+            if (comboBox1.SelectedValue.ToString() == "Все")
+                forecasts = forecastsList;
+            else
+                forecasts = forecastsList.Where(p => p.InventLocationId == comboBox1.SelectedValue.ToString()).ToList();
+
+            return forecasts;
+
+        }
+
+        private void getCalculations(List<forecast> forecasts)
+        {
+            //Avg(qty19)
+            tb19Res.Text = forecasts.Average(p => p.qty19).ToString();
+
+            //Avg(qty20)
+            tb20Res.Text = forecasts.Average(p => p.qty20).ToString();
+
+
+            List<float> f20 = new List<float>();
+
+            //ошибка прогноза avg( abs(QTY20 - FORECAST20)/ QTY20))
+            foreach (var f in getIndex())
+            {
+                if (f.qty20 == 0)
+                    f20.Add(0);
+                else
+                    f20.Add(Math.Abs(f.qty20 - f.forecast20) / f.qty20);
+            }
+
+            forecastRes.Text = f20.Average().ToString();
+
+        }
+
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var frc = new List<forecast>();
+
+            frc = getIndex();
+            forecastGrid.ItemsSource = frc;
+
+            getCalculations(frc);
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Dispose();
+        }
+
+        //сохранит после внесения изменений
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            db.SaveChanges();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (forecastGrid.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < forecastGrid.SelectedItems.Count; i++)
+                {
+                    forecast phone = forecastGrid.SelectedItems[i] as forecast;
+                    if (phone != null)
+                    {
+                        db.forecasts.Remove(phone);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp20/Page2.xaml.cs
@@ -0,0 +1,160 @@
+﻿using LiveCharts;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WpfApp20.Models;
+
+namespace Wpf.CartesianChart.Basic_Bars
+{
+    public partial class BasicColumn : UserControl, INotifyPropertyChanged
+    {
+        forecastContext db;
+
+        private bool _series2019Visibility;
+        private bool _series2020Visibility;
+
+
+        public BasicColumn()
+        {
+            InitializeComponent();
+
+            //ставим на свойства по умолчанию true
+            Series2019Visibility = true;
+            Series2020Visibility = true;
+
+
+            db = new forecastContext();
+            db.forecasts.Load(); // загружаем данные
+
+
+            List<float> l19 = db.forecasts.Select(p => p.qty19).Distinct().ToList();
+            List<float> l20 = db.forecasts.Select(p => p.qty20).Distinct().ToList();
+
+            List<DateTime> listDates;// = new List<DateTime>(); 
+
+            listDates = db.forecasts.Select(p => p.Date).OrderBy(p => p).Distinct().ToList();
+
+            SeriesCollection = new SeriesCollection();
+
+            //Выборка Value1
+            Dictionary<string, float> dateControl = new Dictionary<string, float>();
+
+            foreach (var date in listDates)
+            {
+                var f = db.forecasts.Where(p => p.Date == date && p.InventLocationId.Contains("04_ОСН")).Select(p => p.qty19).ToList();
+                if (f.Count > 0)
+                {
+                    dateControl.Add(date.ToShortDateString(), f[0]);
+                }
+                else
+                    dateControl.Add(date.ToShortDateString(), 0); //нет данных по дате
+            }
+
+            Values1 = dateControl.Values.AsChartValues();
+            dateControl.Clear();
+
+
+            //Выборка Value2
+            foreach (var m in listDates)
+            {
+                var f = db.forecasts.Where(p => p.Date == m && p.InventLocationId.Contains("09_ОСН")).Select(p => p.qty19).ToList();
+                if (f.Count > 0)
+                {
+                    dateControl.Add(m.ToShortDateString(), f[0]);
+                }
+                else
+                    dateControl.Add(m.ToShortDateString(), 0); //нет данных по дате
+            }
+
+            Values2 = dateControl.Values.AsChartValues();
+            dateControl.Clear();
+
+
+            //Выборка Value3
+            foreach (var m in listDates)
+            {
+                var f = db.forecasts.Where(p => p.Date == m && p.InventLocationId.Contains("04_ОСН")).Select(p => p.qty20).ToList();
+                if (f.Count > 0)
+                {
+                    dateControl.Add(m.ToShortDateString(), f[0]);
+                }
+                else
+                    dateControl.Add(m.ToShortDateString(), 0); //нет данных по дате
+            }
+
+            Values3 = dateControl.Values.AsChartValues();
+            dateControl.Clear();
+
+
+            //Выборка Value4
+            foreach (var m in listDates)
+            {
+                var f = db.forecasts.Where(p => p.Date == m && p.InventLocationId.Contains("09_ОСН")).Select(p => p.qty20).ToList();
+                if (f.Count > 0)
+                {
+                    dateControl.Add(m.ToShortDateString(), f[0]);
+                }
+                else
+                    dateControl.Add(m.ToShortDateString(), 0); //нет данных по дате
+            }
+
+            Values4 = dateControl.Values.AsChartValues();
+
+            //список дат на оси x
+            Labels = dateControl.Keys.ToArray();
+            Step = 1;
+
+            DataContext = this;
+        }
+
+
+        public SeriesCollection SeriesCollection { get; set; }
+
+        public ChartValues<float> Values1 { get; set; }
+        public ChartValues<float> Values2 { get; set; }
+        public ChartValues<float> Values3 { get; set; }
+        public ChartValues<float> Values4 { get; set; }
+
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
+        public int Step { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool Series2019Visibility
+        {
+            get { return _series2019Visibility; }
+            set
+            {
+                _series2019Visibility = value;
+                OnPropertyChanged("Series2019Visibility");
+            }
+        }
+
+        public bool Series2020Visibility
+        {
+            get { return _series2020Visibility; }
+            set
+            {
+                _series2020Visibility = value;
+                OnPropertyChanged("Series2020Visibility");
+            }
+        }
+
+
+    }
+}
